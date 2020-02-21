d0256f7db2f3a40d6ad66c40bc6553098eb43010
Zer00CooL <g2pc@visionduweb.com>
2020-02-21 19:30:58 +0100

    #1 Got error 'PHP message: PHP Notice
    if($args["type"] != null){
    
    become
    
    if(!empty($args["type"])){

--- yUML/yUML.php
+++ yUML/yUML.php
@@ -1,48 +1,48 @@
+  GNU nano 4.3                                                                                 /var/www/wiki.visionduweb.fr/extensions/yUML/yUML.php                                                                                 Modifié  
 <?php
 // Author: Sindri Traustason http://sindri.info
- 
-//Avoid unstubbing $wgParser on setHook() too early on modern (1.12+) MW versions, as per r35980
+
+// Avoid unstubbing $wgParser on setHook() too early on modern (1.12+) MW versions, as per r35980
 if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
-	$wgHooks['ParserFirstCallInit'][] = 'efYUMLInit';
-} else { // Otherwise do things the old fashioned way
-	$wgExtensionFunctions[] = 'efYUMLInit';
+        $wgHooks['ParserFirstCallInit'][] = 'efYUMLInit';
+} else {
+        // Otherwise do things the old fashioned way
+        $wgExtensionFunctions[] = 'efYUMLInit';
 }
- 
+
 function efYUMLInit() {
-	global $wgParser;
-	$wgParser->setHook( 'classdiagram', 'efClassdiagramRender' );
-	$wgParser->setHook( 'usecase', 'efUsecaseRender' );
-	return true;
+        global $wgParser;
+        $wgParser->setHook( 'classdiagram', 'efClassdiagramRender' );
+        $wgParser->setHook( 'usecase', 'efUsecaseRender' );
+        return true;
 }
 
 function yUMLRenderDiagram( $input, $args, $diagramType ) {
-	$type = "";
-	if($args["type"] != null){
-		$type = "/".$args["type"];
-	}
-	$scale = "";
-	if($args["scale"] != null){
-		$scale=";scale:".$args["scale"];
-	}
-	$yumldir = "";
-	if($args["dir"] != null){
-		$yumldir=";dir:".$args["dir"];
-	}
-	
-	$uml_code = preg_replace(
-		array("/\n/", "/,,/"),
-		array(", ",   ","   ),
-		trim($input));
-	$output = "<img src=\"http://yUML.me/diagram".$type.$scale.$yumldir."/".$diagramType."/";
-	return $output.htmlspecialchars( $uml_code )."\"/>";
+        $type = "";
+        if(!empty($args["type"])){
+                $type = "/".$args["type"];
+        }
+        $scale = "";
+        if(!empty($args["scale"])){
+                $scale=";scale:".$args["scale"];
+        }
+        $yumldir = "";
+        if(!empty($args["dir"])){
+                $yumldir=";dir:".$args["dir"];
+        }
+        $uml_code = preg_replace(
+                array("/\n/", "/,,/"),
+                array(", ",   ","   ),
+                trim($input));
+        $output = "<img src=\"https://yUML.me/diagram".$type.$scale.$yumldir."/".$diagramType."/";
+        return $output.htmlspecialchars( $uml_code )."\"/>";
 }
-  
+
 function efClassdiagramRender( $input, $args, $parser ) {
-	return yUMLRenderDiagram( $input, $args, "class" );
+        return yUMLRenderDiagram( $input, $args, "class" );
 }
- 
+
 function efUsecaseRender( $input, $args, $parser ) {
-	return yUMLRenderDiagram( $input, $args, "usecase" );
+        return yUMLRenderDiagram( $input, $args, "usecase" );
 }
- 
 ?>
